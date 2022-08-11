4a844eab414517d18ec8f37b04e480e59386f8b3
coalong <minay0904@gmail.com>
2022-08-11 10:03:59 +0900

    Create a Spring Boot application with Maven
    Co-authored-by: CoRock <hoongineer@gmail.com>

---
+++ src/main/java/com/nhnacademy/marketgg/batch/MarketggBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.marketgg.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketggBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketggBatchApplication.class, args);
+    }
+
+}
