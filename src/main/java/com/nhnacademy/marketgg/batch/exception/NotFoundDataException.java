12ad6eb2b8b15bfcfd8432e4565e96874548be26
coalong <minay0904@gmail.com>
2022-08-18 12:09:33 +0900

    Feat: 예외 처리

---
+++ src/main/java/com/nhnacademy/marketgg/batch/exception/NotFoundDataException.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.marketgg.batch.exception;
+
+public class NotFoundDataException extends IllegalArgumentException {
+
+    public NotFoundDataException(String s) {
+        super(s);
+    }
+
+}
