08f488610948b6cb8aee11fdac8dc839ea921ebf
coalong <minay0904@gmail.com>
2022-09-04 19:44:06 +0900

    Fix: typo

--- src/main/java/com/nhnacademy/marketgg/batch/scheduler/CouponIssuanceScheduler.java
+++ src/main/java/com/nhnacademy/marketgg/batch/scheduler/CouponIssuanceScheduler.java
@@ -78,9 +78,8 @@ public class CouponIssuanceScheduler {
      * @since 1.0.0
      */
     @Async
-    // @Scheduled(cron = "@monthly", zone = "Asia/Seoul")
-    @Scheduled(fixedDelay = 100000L)
-    public void scheduleMemberGradeCoupon() throws Exception {
+    @Scheduled(cron = "@monthly", zone = "Asia/Seoul")
+    public void scheduleMemberGradeCoupon() {
         log.info("등급 쿠폰 스케줄러 시작 시간: {}", LocalDateTime.now());
 
         LocalDate previousMonth = LocalDate.now().minusMonths(1L);
